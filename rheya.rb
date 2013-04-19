e510903e71b338aa076fecab2f0aa1872acb3a58
Pandaen <sintpanda@gmail.com>
2013-04-19 14:27:05 +0200

    Foxboron nagging

--- rheya.rb
+++ rheya.rb
@@ -257,10 +257,7 @@ class Quotes
     @db.results_as_hash = true
     @debug = true
     @twit = Twitter::Client.new(
-      :consumer_key => "9x9TByi4BjzXs9N1Oyv3gA",
-      :consumer_secret => "3NfBK7yhwHZLz4ZOAyLZ6aN6amaB55nNCNph48PGs",
-      :oauth_token => "1363095884-N9tj5FR3iFb2Sokhxi59WLxwRoF1AOWPVZ4uydr",
-      :oauth_token_secret => "360KsViDUl7P7ajTmxBYqzNxkW2BnWKAl30Y2Umy4"
+      
     )
     @last_mention = nil
     
